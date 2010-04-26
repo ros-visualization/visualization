f78f963bc4c02015e5ac813a09dd79e235731d7e
Josh Faust <jfaustwg@willowgarage.com>
2010-04-26 20:08:02 +0000

    Fully-functional C++ interface generators with some minimal tests
    git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/branches/visualization-2.0@29101 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- rviz_interface_gen/scripts/gen_cpp.py
+++ rviz_interface_gen/scripts/gen_cpp.py
@@ -52,6 +52,7 @@ def write_header_end(hs, i, pkg):
     
 def write_header_includes(hs, i, pkg):
     print >> hs, '#include <ros/time.h>'
+    print >> hs, '#include <string>'
     for m in i.methods:
         for f in m.fields:
             (base_type, is_array, array_len) = msgs.parse_type(f[0])
@@ -70,11 +71,14 @@ def msg_to_cpp(type):
     else:
         ns = type.split('/')[0]
         c = type.split('/')[1]
-        return '%s::%s'%(ns, c)  
-
-def write_header_method_declaration(hs, i, m, pure_virtual):
-    print >> hs, '  virtual void %s('%(m.name),
+        return '%s::%s'%(ns, c)
     
+def write_header_forward_declarations(hs):
+    print >> hs, 'namespace ros { class NodeHandle; }'
+    print >> hs, 'namespace ros { template<typename M> class MessageEvent; }'
+    print >> hs
+    
+def write_method_args(s, i, m):
     first = True
     for f in m.fields:
         (type, _, _) = msgs.parse_type(f[0])
@@ -83,13 +87,13 @@ def write_header_method_declaration(hs, i, m, pure_virtual):
         if (first):
            first = False
         else:
-            print >> hs, ', ',
+            print >> s, ', ',
         
         cpp_type = msg_to_cpp(type)
         if (msgs.is_valid_constant_type(type)):
-            print >> hs, '%s %s'%(cpp_type, name),
+            print >> s, '%s %s'%(cpp_type, name),
         else:
-            print >> hs, 'const %s& %s'%(cpp_type, name),
+            print >> s, 'const %s& %s'%(cpp_type, name),
             
     for f in m.return_fields:
         (type, _, _) = msgs.parse_type(f[0])
@@ -98,10 +102,15 @@ def write_header_method_declaration(hs, i, m, pure_virtual):
         if (first):
            first = False
         else:
-            print >> hs, ', ',
+            print >> s, ', ',
         
         cpp_type = msg_to_cpp(type)
-        print >> hs, '%s& out_%s'%(cpp_type, name),
+        print >> s, '%s& out_%s'%(cpp_type, name),
+
+def write_header_method_declaration(hs, i, m, pure_virtual):
+    print >> hs, '  virtual void %s('%(m.name),
+    
+    write_method_args(hs, i, m)
              
     print >> hs, ')',
     if (pure_virtual):
@@ -120,10 +129,9 @@ def write_header_interface_declaration(hs, i, pkg):
     print >> hs, '};\n'
     
 def write_header_proxy_declaration(hs, i, pkg):
-    print >> hs, 'struct %sProxy\n{'%(i.name)
+    print >> hs, 'struct %sProxy : public %s\n{'%(i.name, i.name)
     
-    print >> hs, '  %sProxy();'%(i.name)
-    print >> hs, '  void connect(const std::string& name, const ros::NodeHandle& nh);'
+    print >> hs, '  %sProxy(const std::string& name, const ros::NodeHandle& nh);'%(i.name)
     
     for m in i.methods:
         write_header_method_declaration(hs, i, m, False)
@@ -136,12 +144,9 @@ def write_header_proxy_declaration(hs, i, pkg):
     print >> hs, '};\n'
     
 def write_header_server_declaration(hs, i, pkg):
-    print >> hs, 'struct %sServer\n{'%(i.name)
+    print >> hs, 'struct %sServer : public %s\n{'%(i.name, i.name)
     
-    print >> hs, '  %sServer();'%(i.name)
-    
-    for m in i.methods:
-        write_header_method_declaration(hs, i, m, False)
+    print >> hs, '  %sServer(const std::string& name, const ros::NodeHandle& nh);'%(i.name)
         
     print >> hs, 'private:'
     print >> hs, '  struct Impl;'
@@ -150,11 +155,121 @@ def write_header_server_declaration(hs, i, pkg):
     
     print >> hs, '};\n'
     
-def write_cpp_begin(cpps, i, pkg):
-    pass
+    
+def write_cpp_includes(cpps, i, pkg):
+    print >> cpps, '#include <%s/%s.h>'%(pkg, i.name)
+    print >> cpps, '#include <rviz_rpc/client.h>'
+    print >> cpps, '#include <rviz_rpc/server.h>'
+    
+    for m in i.methods:
+        print >> cpps, '#include <%s/%s_%sRequest.h>'%(pkg, i.name, m.name)
+        print >> cpps, '#include <%s/%s_%sResponse.h>'%(pkg, i.name, m.name)
+        
+    print >> cpps
+    print >> cpps, 'using namespace %s;\n'%(pkg)
 
-def write_cpp_end(cpps, i, pkg):
-    pass
+def write_cpp_proxy_method(cpps, i, m, pkg):
+    print >> cpps, 'void %sProxy::%s('%(i.name, m.name),
+    write_method_args(cpps, i, m)
+    print >> cpps, ')',
+    cpp_prefix = cpp_message_prefix(pkg, i, m)
+    print >> cpps, """
+{
+  %sRequestPtr req(new %sRequest);
+"""%(cpp_prefix, cpp_prefix)
+
+    for field in m.fields:
+        print >> cpps, '  req->%s = %s;'%(field[1], field[1])
+        
+    if ('async' in m.attributes):
+        if (len(m.return_fields) > 0):
+            raise Error('Calls with return values cannot be async (method %s_%s)'%(i.name, m.name))
+        
+        print >> cpps, '  impl_->%s_method_.callAsync(req);'%(m.name)
+    else:
+        print >> cpps, '  %sResponseConstPtr res = impl_->%s_method_.call(req);'%(cpp_prefix, m.name)
+        for field in m.return_fields:
+            print >> cpps, '  out_%s = res->%s;'%(field[1], field[1])
+            
+    print >> cpps, '}\n'
+
+def cpp_message_prefix(pkg, i, m):
+    return '%s::%s_%s'%(pkg, i.name, m.name)
+
+def write_cpp_proxy_definition(cpps, i, pkg):    
+    print >> cpps, 'struct %sProxy::Impl\n{'%(i.name)
+    print >> cpps, """ 
+  Impl(const std::string& name, const ros::NodeHandle& nh)
+  : client_(name, nh)
+  {
+  }
+"""
+
+    print >> cpps, '  rviz_rpc::Client client_;'
+    for m in i.methods:
+        print >> cpps, '  rviz_rpc::Method<%sRequest, %sResponse> %s_method_;'%(cpp_message_prefix(pkg, i, m), cpp_message_prefix(pkg, i, m), m.name)
+    print >> cpps, '};\n'
+    
+    print >> cpps, '%sProxy::%sProxy(const std::string& name, const ros::NodeHandle& nh)'%(i.name, i.name)
+    print >> cpps, ': impl_(new Impl(name, nh))\n{'
+    for m in i.methods:
+        print >> cpps, '  impl_->%s_method_ = impl_->client_.addMethod<%sRequest, %sResponse>("%s");'%(m.name, cpp_message_prefix(pkg, i, m), cpp_message_prefix(pkg, i, m), m.name)
+        
+    print >> cpps, '  impl_->client_.connect();'
+    print >> cpps, '}\n'
+    
+    for m in i.methods:
+        write_cpp_proxy_method(cpps, i, m, pkg)
+        
+def write_cpp_server_definition(cpps, i, pkg):
+    print >> cpps, 'struct %sServer::Impl\n{'%(i.name)
+    print >> cpps, """
+  Impl(const std::string& name, const ros::NodeHandle& nh, %sServer* parent)
+  : server_(name, nh)
+  , parent_(parent)
+  {
+  }
+"""%(i.name)
+
+    for m in i.methods:
+        cpp_prefix = cpp_message_prefix(pkg, i, m)
+        print >> cpps, '  %sResponseConstPtr %s_callback(const ros::MessageEvent<%sRequest>& evt)\n  {'%(cpp_prefix, m.name, cpp_prefix)
+        print >> cpps, '    %sRequestPtr req = evt.getMessage();'%(cpp_prefix)
+        print >> cpps, '    %sResponsePtr res(new %sResponse);'%(cpp_prefix, cpp_prefix)
+        print >> cpps, '    parent_->%s('%(m.name),
+        first = True
+        for f in m.fields:
+            if (first):
+                first = False
+            else:
+                print >> cpps, ',',
+            print >> cpps, 'req->%s'%(f[1]),
+            
+        for f in m.return_fields:
+            if (first):
+                first = False
+            else:
+                print >> cpps, ',',
+            
+            print >> cpps, 'res->%s'%(f[1]),
+        print >> cpps, ');\n'
+        print >> cpps, '    return res;'
+        print >> cpps, '  }'
+
+    print >> cpps
+    print >> cpps, '  rviz_rpc::Server server_;'
+    print >> cpps, '  %sServer* parent_;'%(i.name)
+    print >> cpps, '};\n'
+    
+    print >> cpps, '%sServer::%sServer(const std::string& name, const ros::NodeHandle& nh)'%(i.name, i.name)
+    print >> cpps, ': impl_(new Impl(name, nh, this))'
+    print >> cpps, '{'
+    for m in i.methods:
+        cpp_prefix = cpp_message_prefix(pkg, i, m)
+        print >> cpps, '  impl_->server_.addMethod<%sRequest, %sResponse>("%s", boost::bind(&Impl::%s_callback, impl_.get(), _1));'%(cpp_prefix, cpp_prefix, m.name, m.name)
+    print >> cpps, '  impl_->server_.ready();'
+    print >> cpps, '}'
+    
 
 def generate(output_dir, i, pkg, pkg_path):
     hs = StringIO()
@@ -162,6 +277,8 @@ def generate(output_dir, i, pkg, pkg_path):
     
     write_header_begin(hs, i, pkg)
     write_header_includes(hs, i, pkg)
+    
+    write_header_forward_declarations(hs)
     
     print >> hs, 'namespace %s\n{\n'%(pkg)
     
@@ -173,8 +290,9 @@ def generate(output_dir, i, pkg, pkg_path):
     
     write_header_end(hs, i, pkg)
     
-    write_cpp_begin(cpps, i, pkg)
-    write_cpp_end(cpps, i, pkg)
+    write_cpp_includes(cpps, i, pkg)
+    write_cpp_proxy_definition(cpps, i, pkg)
+    write_cpp_server_definition(cpps, i, pkg)
     
     header_dir = '%s/include/%s'%(output_dir, pkg)
     cpp_dir = '%s/src'%(output_dir)
---
+++ rviz_interface_gen/test/test_interface.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2010, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <rviz_interface_gen/Test.h>
+
+#include <gtest/gtest.h>
+
+#include <ros/ros.h>
+
+using namespace rviz_interface_gen;
+
+class MyTestServer : public TestServer
+{
+public:
+  MyTestServer(const std::string& name, const ros::NodeHandle& nh)
+  : TestServer(name, nh)
+  {}
+
+  virtual void triple( uint32_t val ,  uint32_t& out_val )
+  {
+    out_val = val * 3;
+  }
+
+  virtual void blockFor( const ros::Duration& d )
+  {
+    d.sleep();
+  }
+
+  virtual void noBlock( )
+  {
+    async_call_done = false;
+    ros::Duration(1.0).sleep();
+    async_call_done = true;
+  }
+
+  virtual void complex( const std_msgs::String& name ,  float val ,  std_msgs::String& out_ret )
+  {
+    std::stringstream ss;
+    ss << name << val;
+    out_ret.data = ss.str();
+  }
+
+  virtual void multipleReturn(uint32_t val, uint32_t& out_val, uint32_t& out_val_doubled)
+  {
+    out_val = val;
+    out_val_doubled = val * 2;
+  }
+
+  bool async_call_done;
+};
+
+TEST(Interfaces, callWithReturn)
+{
+  ros::AsyncSpinner sp(1);
+  sp.start();
+  ros::NodeHandle nh;
+  MyTestServer s("test", nh);
+  TestProxy p("test", nh);
+
+  uint32_t ret = 0;
+  p.triple(5, ret);
+  EXPECT_EQ(ret, 15U);
+}
+
+TEST(Interfaces, blockingCall)
+{
+  ros::AsyncSpinner sp(1);
+  sp.start();
+  ros::NodeHandle nh;
+  MyTestServer s("test", nh);
+  TestProxy p("test", nh);
+
+  ros::Time start = ros::Time::now();
+  p.blockFor(ros::Duration(1.0));
+  ros::Time end = ros::Time::now();
+  EXPECT_GE(end, start + ros::Duration(1.0));
+}
+
+TEST(Interfaces, asyncCall)
+{
+  ros::AsyncSpinner sp(1);
+  sp.start();
+  ros::NodeHandle nh;
+  MyTestServer s("test", nh);
+  TestProxy p("test", nh);
+
+  p.noBlock();
+  EXPECT_FALSE(s.async_call_done);
+}
+
+TEST(Interfaces, multipleReturn)
+{
+  ros::AsyncSpinner sp(1);
+  sp.start();
+  ros::NodeHandle nh;
+  MyTestServer s("test", nh);
+  TestProxy p("test", nh);
+
+  uint32_t ret1 = 0;
+  uint32_t ret2 = 0;
+  p.multipleReturn(5, ret1, ret2);
+  EXPECT_EQ(ret1, 5U);
+  EXPECT_EQ(ret2, 10U);
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "test_intra");
+  ros::NodeHandle nh;
+
+  return RUN_ALL_TESTS();
+}
+
