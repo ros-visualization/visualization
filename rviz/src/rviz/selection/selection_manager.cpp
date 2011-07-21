ros-visualization/visualization

commit ea63d407dc781f0610ebeb510015ba487fc321ab

rviz: fixed bug #5072, selection only worked for large areas.
git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@37613 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

rviz/src/rviz/selection/selection_manager.cpp
@@ -673,7 +673,9 @@ bool SelectionManager::render(Ogre::Viewport* viewport, Ogre::TexturePtr tex,
 
   // set viewport to render to a subwindow of the texture
   Ogre::Viewport* render_viewport = render_texture->getViewport(0);
-  render_viewport->setDimensions(0,0,round((float)render_w / (float)texture_size_),round((float)render_h / (float)texture_size_));
+  render_viewport->setDimensions( 0, 0,
+                                  (float)render_w / (float)texture_size_,
+                                  (float)render_h / (float)texture_size_ );
 
   ros::WallTime start = ros::WallTime::now();
 

commit 81e82b20ee2ee561e0de5c0e0ce0ef2795a000df

rviz: fixed bug in 3D mouse click position finding introduced in r37613
git-svn-id: https://code.ros.org/svn/ros-pkg/stacks/visualization/trunk@37651 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

rviz/src/rviz/selection/selection_manager.cpp
@@ -163,11 +163,11 @@ void SelectionManager::initDepthFinder()
     Ogre::TextureManager::getSingleton().remove( tex_name );
   }
 
-  int depth_texture_size = 1;
+  depth_texture_size_ = 1;
   depth_render_texture_ =
     Ogre::TextureManager::getSingleton().createManual( tex_name,
                                                        Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
-                                                       Ogre::TEX_TYPE_2D, depth_texture_size, depth_texture_size, 0,
+                                                       Ogre::TEX_TYPE_2D, depth_texture_size_, depth_texture_size_, 0,
                                                        Ogre::PF_R8G8B8,
                                                        Ogre::TU_RENDERTARGET );
   Ogre::RenderTexture* render_texture = depth_render_texture_->getBuffer()->getRenderTarget();
@@ -239,7 +239,7 @@ bool SelectionManager::get3DPoint( Ogre::Viewport* viewport, int x, int y, Ogre:
   }
 
   bool success = false;
-  if( render(viewport, depth_render_texture_, x, y, x + 1, y + 1, depth_pixel_box_, "Depth") )
+  if( render( viewport, depth_render_texture_, x, y, x + 1, y + 1, depth_pixel_box_, "Depth", depth_texture_size_ ))
   {
     uint8_t* data_ptr = (uint8_t*) depth_pixel_box_.data;
     uint8_t a = *data_ptr++;
@@ -572,7 +572,7 @@ void SelectionManager::renderAndUnpack(Ogre::Viewport* viewport, uint32_t pass, 
     scheme << pass;
   }
 
-  if( render(viewport, render_textures_[pass], x1, y1, x2, y2, pixel_boxes_[pass], scheme.str()) )
+  if( render( viewport, render_textures_[pass], x1, y1, x2, y2, pixel_boxes_[pass], scheme.str(), texture_size_ ))
   {
     unpackColors(pixel_boxes_[pass], pixels);
   }
@@ -580,7 +580,8 @@ void SelectionManager::renderAndUnpack(Ogre::Viewport* viewport, uint32_t pass, 
 
 bool SelectionManager::render(Ogre::Viewport* viewport, Ogre::TexturePtr tex,
                               int x1, int y1, int x2, int y2,
-                              Ogre::PixelBox& dst_box, std::string material_scheme )
+                              Ogre::PixelBox& dst_box, std::string material_scheme,
+                              int texture_size)
 {
   vis_manager_->lockRender();
 
@@ -653,30 +654,30 @@ bool SelectionManager::render(Ogre::Viewport* viewport, Ogre::TexturePtr tex,
 
   if ( w>h )
   {
-    if ( render_w > texture_size_ )
+    if ( render_w > texture_size )
     {
-      render_w = texture_size_;
-      render_h = round( float(h) * (float)texture_size_ / (float)w );
+      render_w = texture_size;
+      render_h = round( float(h) * (float)texture_size / (float)w );
     }
   }
   else
   {
-    if ( render_h > texture_size_ )
+    if ( render_h > texture_size )
     {
-      render_h = texture_size_;
-      render_w = round( float(w) * (float)texture_size_ / (float)h );
+      render_h = texture_size;
+      render_w = round( float(w) * (float)texture_size / (float)h );
     }
   }
 
   // safety clamping in case of rounding errors
-  if ( render_w > texture_size_ ) render_w = texture_size_;
-  if ( render_h > texture_size_ ) render_h = texture_size_;
+  if ( render_w > texture_size ) render_w = texture_size;
+  if ( render_h > texture_size ) render_h = texture_size;
 
   // set viewport to render to a subwindow of the texture
   Ogre::Viewport* render_viewport = render_texture->getViewport(0);
   render_viewport->setDimensions( 0, 0,
-                                  (float)render_w / (float)texture_size_,
-                                  (float)render_h / (float)texture_size_ );
+                                  (float)render_w / (float)texture_size,
+                                  (float)render_h / (float)texture_size );
 
   ros::WallTime start = ros::WallTime::now();
 

rviz/src/rviz/selection/selection_manager.h
@@ -139,9 +139,9 @@ public:
   // change the size of the off-screen selection buffer texture
   void setTextureSize( unsigned size );
 
-  // Return true if the point at x, y in the viewport is showing an
-  // object, false otherwise.  If it is showing an object, result will
-  // be changed to contain the 3D point corresponding to it.
+  /** Return true if the point at x, y in the viewport is showing an
+   * object, false otherwise.  If it is showing an object, result will
+   * be changed to contain the 3D point corresponding to it. */
   bool get3DPoint( Ogre::Viewport* viewport, int x, int y, Ogre::Vector3& result_point );
 
   // Implementation for Ogre::RenderQueueListener.
@@ -154,11 +154,18 @@ protected:
   void removeSelection(const Picked& obj);
 
   void setHighlightRect(Ogre::Viewport* viewport, int x1, int y1, int x2, int y2);
+
+  /** Render to a texture for one of the picking passes and unpack the resulting pixels. */
   void renderAndUnpack(Ogre::Viewport* viewport, uint32_t pass, int x1, int y1, int x2, int y2, V_Pixel& pixels);
+
+  /** Internal render function to render to a texture and read the pixels back out. */
   bool render( Ogre::Viewport* viewport, Ogre::TexturePtr tex,
                int x1, int y1, int x2, int y2,
-               Ogre::PixelBox& dst_box, std::string material_scheme );
+               Ogre::PixelBox& dst_box, std::string material_scheme,
+               int texture_size );
+
   void unpackColors(const Ogre::PixelBox& box, V_Pixel& pixels);
+
   void initDepthFinder();
 
   // Set the visibility of the debug windows.  If debug_mode_ is false, this has no effect.
@@ -191,6 +198,7 @@ protected:
 
   // Graphics card -based depth finding of clicked points.
   Ogre::TexturePtr depth_render_texture_;
+  int depth_texture_size_;
   Ogre::SceneNode* debug_depth_node_;
   Ogre::MaterialPtr debug_depth_material_;
   Ogre::PixelBox depth_pixel_box_;